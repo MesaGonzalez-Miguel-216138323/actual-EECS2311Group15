6e3d71003705dbe5d51b38bd323e7b2914bb4928
Ayub <aosman30@my.yorku.ca>
2021-01-27 13:12:43 -0500

    working parseer.

--- EEC2311_Project_TABTOXML/src/main/java/tabToXml/GuitarTab.java
+++ EEC2311_Project_TABTOXML/src/main/java/tabToXml/GuitarTab.java
@@ -1,5 +1,59 @@
 package tabToXml;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class GuitarTab {
+	
+	private File inputFile;
+	private ArrayList<String> tab = new ArrayList<>();
+	
+	public GuitarTab(String inputFile) 
+	{
+		this.inputFile = new File(inputFile);
+		this.readFile();
+	}
+	
+	private void readFile() 
+	{
+		Scanner sc = null;
+		try 
+		{
+			sc = new Scanner(inputFile);
+			while(sc.hasNextLine()) 
+			{
+				String test = sc.nextLine();
+				if(test.contains("-") && test.contains("|"))
+					tab.add(test);
+			}
+			
+		}
+		catch(FileNotFoundException e) 
+		{
+			e.printStackTrace();
+		}
+		finally 
+		{
+			sc.close();
+		}
+	}
+	
+	@Override
+	public String toString() 
+	{
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(String s : tab) 
+		{
+			sb.append(s + "\n");
+		}
+		return sb.toString();
+		
+	}
+	
+	
 
 }
