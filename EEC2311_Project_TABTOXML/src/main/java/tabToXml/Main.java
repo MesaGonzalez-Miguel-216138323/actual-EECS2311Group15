dca9e8ce713b01eb8ef2cf4b9a67fdaa1822c982
Elijah Nnorom <elyjannzira@gmail.com>
2021-02-01 11:23:16 -0500

    Reading into 2d array

--- EEC2311_Project_TABTOXML/src/main/java/tabToXml/Main.java
+++ EEC2311_Project_TABTOXML/src/main/java/tabToXml/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	public static TextFileReader test = new TextFileReader("C:\\Users\\Elijah\\git\\actual-EECS2311Group15\\EEC2311_Project_TABTOXML\\src\\main\\java\\tabToXml\\TabFile.txt");
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -17,9 +18,18 @@ public class Main extends Application {
 		primaryStage.show();
 	}
 	
+//	public static void conversion() {
+//		for (int j = 0; j < test.printList().size(); j++) {
+//			System.out.print(test.printList().get(0).get(0));
+//		}
+//		
+//	}
+	
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
-		//GuitarTab gt = new GuitarTab("tab.txt");
+		System.out.print(test.printList().get(0).get(0));
+		//conversion();
 		System.out.print("Launching Application");
 		launch(args);
 	}
--- EEC2311_Project_TABTOXML/src/main/java/tabToXml/TextFileReader.java
+++ EEC2311_Project_TABTOXML/src/main/java/tabToXml/TextFileReader.java
@@ -3,26 +3,43 @@ package tabToXml;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class TextFileReader {
 	
 	private File inputFile;
-	private ArrayList<String> tab = new ArrayList<>();
+	//private ArrayList<String> tab = new ArrayList<>();
+	private List<List<String>> lists = new ArrayList<List<String>>();
 	
 	public TextFileReader(String inputFile){
 		this.inputFile = new File(inputFile);
 		this.readFile();
 	}
 	
+	
 	private void readFile(){
 		Scanner sc = null;
 		try {
 			sc = new Scanner(inputFile);
+			List<String> list = new ArrayList<>();
+			
+			String previousLine = "";
 			while(sc.hasNextLine()){
-				String test = sc.nextLine();
-				// if(test.contains("-") && test.contains("|"))
-				tab.add(test);
+				
+				String line = sc.nextLine();
+				if (!(previousLine.isEmpty()) && (line.contains("-") && line.contains("|"))) {
+					list.add(line);
+					
+				}
+				
+				if ((previousLine.contains("-") && previousLine.contains("|")) && !(line.contains("-") && line.contains("|"))) {					
+					lists.add(list);
+					list = new ArrayList<>();					
+					
+				}		
+				previousLine = line;
+				
 			}		
 		}
 		catch(FileNotFoundException e) {
@@ -33,12 +50,16 @@ public class TextFileReader {
 		}
 	}
 	
-	@Override
-	public String toString() {		
-		StringBuilder sb = new StringBuilder();		
-		for(String s : tab) {
-			sb.append(s + "\n");
-		}
-		return sb.toString();		
+
+	public List<List<String>> printList() {		
+		//StringBuilder sb = new StringBuilder();	
+		
+//		for(List<String> s : lists) {
+//			sb.append(s + "\n");
+//		}
+		//return sb.toString();	
+		
+	//	System.out.print(lists);
+		return lists;
 	}
 }
