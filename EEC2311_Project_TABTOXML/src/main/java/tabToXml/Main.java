7fd30399ae1934730b4db88d7d37738fad3eb4a9
Ayub <aosman30@my.yorku.ca>
2021-02-03 12:17:22 -0500

    Revert "Merge branch 'develop' into Ayub_feature"
    This reverts commit ca63ddd7eac4c5eeca8745327225691748bbfcf5.

--- EEC2311_Project_TABTOXML/src/main/java/tabToXml/Main.java
+++ EEC2311_Project_TABTOXML/src/main/java/tabToXml/Main.java
@@ -20,9 +20,9 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		System.out.print("Launching Application");
 		launch(args);
-		//useless comment
-		// Lab Task
 	}
+
+  
 }
 
 
